cd79c7c26e59e7cfc6732f26e91dd6ff54dc5ef6
shawn-exe <112694633+shawn-exe@users.noreply.github.com>
2023-12-03 12:34:21 +0530

    Links fixed

--- client/src/components/Navbar/navlinks.jsx
+++ client/src/components/Navbar/navlinks.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 function navlinks() {
   return (
     <div className='flex flex-row justify-center gap-20 mr-40 '>
-        <a href="/"  className="flex flex-shrink-0 no-underline text-black">Home</a>
+        <Link to="/"  className="flex flex-shrink-0 no-underline text-black">Home</Link>
         <Link to="/aboutus"  className="flex flex-shrink-0 no-underline text-black">About us</Link>
-        <a href="/" className="flex  flex-shrink-0 no-underline text-black" >Contact us</a>
+        <Link to="/contactus" className="flex  flex-shrink-0 no-underline text-black" >Contact us</Link>
     </div>
   )
 }
--- client/src/components/Navbar/navlinkslog.jsx
+++ client/src/components/Navbar/navlinkslog.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom'
 function navlinks() {
   return (
     <div className='flex flex-row justify-center items-center gap-20 mr-40 '>
-        <Link to="/"  className="flex flex-shrink-0 no-underline text-black">Home</Link>
+      <Link to="/"  className="flex flex-shrink-0 no-underline text-black">Home</Link>
         <Link to="/aboutus"  className="flex flex-shrink-0 no-underline text-black">About us</Link>
-        <Link to="/" className="flex  flex-shrink-0 no-underline text-black" >Contact us</Link>
+        <Link to="/contactus" className="flex  flex-shrink-0 no-underline text-black" >Contact us</Link>
         <Logout />
     </div>
   )
